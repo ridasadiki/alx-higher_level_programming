6dca2325170553d1043137dbe26b696ba9fd43f0
ridasadiki <137537627+ridasadiki@users.noreply.github.com>
2023-10-07 02:58:24 +0200

    Delete

--- 5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    new_str = ""
-    for i in range(len(my_string)):
-        if my_string[i] == 'C' or my_string[i] == 'c':
-            pass
-        else:
-            new_str += my_string[i]
-    return new_str
